9e103b905c6f6f59986a038f114a2c6bcd4b02af
cumsud-ismayilov <c.smayilov1@gmail.com>
2025-07-23 14:16:39 +0400

    react file structure add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Layout/Navbar/Navbar";
+
+function App() {
+  return (
+    <div >
+      <Navbar/>
+      <p className="text-3xl font-bold underline">salam</p>
+    </div>
+  )
+}
+
+export default App

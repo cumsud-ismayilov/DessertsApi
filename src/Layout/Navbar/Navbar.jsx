a675743c10ca3647f569b3fc98b92a14c928dc8a
cumsud-ismayilov <c.smayilov1@gmail.com>
2025-07-24 19:29:39 +0400

    pageLogo add

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar({setSearchVal}) {
   }
   return (
     <header className="flex justify-between items-center  px-[7rem] mb-[50px] shadow-lg">
-      <img src="/public/pageLogo.png" alt="pagelogo" className="w-20 h-20 object-contain"/>
+      <img src="/pageLogo.png" alt="pagelogo" className="w-20 h-20 object-contain"/>
       <input type="text" placeholder="Search desserts..." className="shadow-md outline-none py-[7px] px-[30px] border-[#FFB703] border-2 rounded-md text-[#2D3142]" onChange={inputHandler}/>
     </header>
   )
